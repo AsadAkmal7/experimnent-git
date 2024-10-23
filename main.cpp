a5d82c35a314aa6278cdd13ef1dc48ecca3d94e3
asad <asad.akmal2@gmail.com>
2024-10-23 22:16:47 +0500

    init commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std ;
+int main() {
+    float weight, bmi, height;
+    cout << "Enter your weight in Kg :";
+    cin >> weight;
+    cout << "Enter height in centimeters :";
+     cin >> height;
+     height = height/100;
+     height = height * height;
+    bmi = weight/height;
+    cout << bmi;
+
+    return 0;
+}
